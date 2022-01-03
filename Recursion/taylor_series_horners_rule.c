ab767b14de75323a89a979c832b1fd74f0439518
Nisarg6502 <nisarg6502@gmail.com>
2022-01-04 00:04:15 +0530

    taylor series horner's rule

---
+++ Recursion/taylor_series_horners_rule.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+double e(int x, int n)
+{
+    static double s;
+    if (n == 0)
+    {
+        return s;
+    }
+    s = 1 + x * s / n;
+    return e(x, n - 1);
+}
+double e1(int x, int n)
+{
+    double s = 1, den = 1, num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        num *= x;
+        den *= i;
+        s += num / den;
+    }
+    return s;
+}
+
+int main()
+{
+    printf("%lf\n", e(2, 10));
+    printf("%lf\n", e1(2, 10));
+    return 0;
+}
