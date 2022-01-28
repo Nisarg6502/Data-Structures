0c1a0b0d1079ff22eea8d82b3946d60153c1c703
Nisarg6502 <nisarg6502@gmail.com>
2022-01-29 01:09:30 +0530

    finding single missing element

---
+++ Array ADT/finding_single_missing_element.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array
+{
+    int *A;
+    int size;
+};
+
+void Display(struct Array arr)
+{
+    for (int i = 0; i < arr.size; i++)
+    {
+        printf("%d\t", arr.A[i]);
+    }
+}
+
+void MissingElement(struct Array arr)
+{
+    int h = arr.A[arr.size - 1], l = arr.A[0];
+    int diff = l - 0;
+    for (int i = 0; i < arr.size; i++)
+    {
+        if (arr.A[i] - i != diff)
+        {
+            printf("The missing element is: %d", diff + i);
+            break;
+        }
+    }
+}
+
+int main()
+{
+    struct Array arr;
+    printf("Enter number of elements");
+    scanf("%d", &arr.size);
+    printf("\nnumber of elements %d", arr.size);
+    arr.A = (int *)malloc(arr.size * sizeof(int));
+    printf("\nEnter elements");
+    for (int i = 0; i < arr.size; i++)
+    {
+        scanf("%d", &arr.A[i]);
+    }
+    Display(arr);
+    MissingElement(arr);
+    return 0;
+}
