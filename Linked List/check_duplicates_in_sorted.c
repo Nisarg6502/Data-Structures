10e0579eedaa2e93e82f321ddc1a70b19b382d52
Nisarg6502 <nisarg6502@gmail.com>
2022-02-11 23:54:03 +0530

    check duplicates in sorted linked list

---
+++ Linked List/check_duplicates_in_sorted.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} * first;
+
+void Create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Duplicate(struct Node *p)
+{
+    struct Node *q;
+    q = first->next;
+    while (q)
+    {
+        if (p->data == q->data)
+        {
+            p->next = q->next;
+            free(q);
+            q = p->next;
+        }
+        else
+        {
+            p = q;
+            q = q->next;
+        }
+    }
+}
+
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+int main()
+{
+    int A[] = {6, 13, 13, 1989, 2002, 15};
+    Create(A, 6);
+    Display(first);
+    printf("\n\n");
+    Duplicate(first);
+    Display(first);
+    return 0;
+}
