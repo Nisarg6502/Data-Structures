2cdcf20c3f6d4c82606a49c343d6f2a204ea9eee
Nisarg6502 <nisarg6502@gmail.com>
2022-02-12 15:58:54 +0530

    concatenation and merging

---
+++ Linked List/concatenation_and_merging.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} * first, *second = NULL, *third = NULL;
+
+void Create1(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+void Create2(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    second = (struct Node *)malloc(sizeof(struct Node));
+    second->data = A[0];
+    second->next = NULL;
+    last = second;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+int Count(struct Node *p)
+{
+    int count = 0;
+    while (p)
+    {
+        count++;
+        p = p->next;
+    }
+    return count;
+}
+
+void Concate(struct Node *p, struct Node *q)
+{
+    third = p;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    p->next = q;
+}
+
+void Merge(struct Node *p, struct Node *q)
+{
+    struct Node *last;
+    if (p->data < q->data)
+    {
+        third = last = p;
+        p = p->next;
+        third->next = NULL;
+    }
+    else
+    {
+        third = last = q;
+        q = q->next;
+        third->next = NULL;
+    }
+    while (p && q)
+    {
+        if (p->data < q->data)
+        {
+            last->next = p;
+            last = p;
+            p = p->next;
+            last->next = NULL;
+        }
+        else
+        {
+            last->next = q;
+            last = q;
+            q = q->next;
+            last->next = NULL;
+        }
+    }
+
+    if (p)
+    {
+        last->next = p;
+    }
+    if (q)
+    {
+        last->next = q;
+    }
+}
+
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+int main()
+{
+    int A[] = {6, 13, 13, 1989, 2002};
+    int B[] = {1, 4, 14, 65, 2000};
+    Create1(A, 5);
+    Create2(B, 5);
+    Display(first);
+    printf("\n\n");
+    Display(second);
+    // Concate(first, second);
+    printf("\n\n");
+    Display(third);
+    Merge(first, second);
+    printf("\n\n");
+    Display(third);
+    return 0;
+}
